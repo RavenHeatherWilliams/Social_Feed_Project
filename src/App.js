736702ccd942944bad6649d6e637319bc48b2497
Raven Williams <heatherwilliams.raven@gmail.com>
2023-01-03 17:36:02 -0600

    Created a components folder to host DisplayPosts.jsx

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { useState } from 'react';
 
-
+// under const add date option
 function App() {
-  const[entries, setEntries] = useState([{name:"Heather", body: "Hi World!"}])
+  const[entries, setEntries] = useState([{name:"Heather", body: "Hi World!", date:"12-31-2022"}])
   return (
     <div>
       <table>
         <thead>
+          {/* <th>Entry Number</th> */}
           <th>Name</th>
           <th>Body</th>
+          <th>Date</th>
         </thead>
         <tbody>
-          {entries.map((entry) => {
+          {entries.map((entry, index) => {
             return (
-              <tr><td>{entry.name}</td><td>{entry.body}</td></tr>
+              <tr>
+                {/* <td>{index+1}</td> */}
+                <td>{entry.name}</td>
+                <td>{entry.body}</td>
+                <td>{entry.date}</td>
+              </tr>
             )
             })}
         </tbody>
