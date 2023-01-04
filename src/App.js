918871e619cdd383de9d6d71485592a584818534
Raven Williams <heatherwilliams.raven@gmail.com>
2023-01-03 18:02:13 -0600

    Changed name from DisplayPosts to DisplayApp to make DisplayApp the parent + instantiated Components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import React, { useState } from 'react';
+import DisplayApp from './Components/DisplayApp/DisplayApp'
 
 // under const add date option
 function App() {
   const[entries, setEntries] = useState([{name:"Heather", body: "Hi World!", date:"12-31-2022"}])
+
   return (
     <div>
-      <table>
-        <thead>
-          {/* <th>Entry Number</th> */}
-          <th>Name</th>
-          <th>Body</th>
-          <th>Date</th>
-        </thead>
-        <tbody>
-          {entries.map((entry, index) => {
-            return (
-              <tr>
-                {/* <td>{index+1}</td> */}
-                <td>{entry.name}</td>
-                <td>{entry.body}</td>
-                <td>{entry.date}</td>
-              </tr>
-            )
-            })}
-        </tbody>
-      </table>
+      <DisplayApp parentEntries={entries} />
 
     </div>
   );
